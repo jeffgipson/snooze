3b476255d30e78103d6d084ee1368c4b6377fc9e
jeffgipson <jeff@recruiterswebsites.com>
2020-09-08 11:26:13 -0500

    vladimirs changes

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -22,6 +22,11 @@ class SignupsController < ApplicationController
     require 'openssl'
     require 'json'
 
+    org_id = 3240
+    user_id = 1287178
+    cred_id = 644272
+    entry_id = 15423
+
     url = URI("https://api.openpath.com/auth/login")
 
     http = Net::HTTP.new(url.host, url.port)
@@ -33,11 +38,49 @@ class SignupsController < ApplicationController
     request["content-type"] = 'application/json'
     request.body = "{\"email\":\"jeff@recruiterswebsites.com\",\"password\":\"Placement123\"}"
 
-    @response = http.request(request)
+   @response = http.request(request)
     puts @response.read_body
 
     body = JSON.parse(@response.read_body)
     @token = body['data']['token']
+
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    url1 = URI("https://api.openpath.com/orgs/#{org_id}/users/#{user_id}/credentials/#{cred_id}/generateCloudKeyUnlockToken")
+
+    http1 = Net::HTTP.new(url1.host, url1.port)
+    http1.use_ssl = true
+    http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request1 = Net::HTTP::Post.new(url1)
+    request1["accept"] = 'application/json'
+    request1["content-type"] = 'application/json'
+    request1["authorization"] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eUlkIjoxMjg5Mjk0LCJybmQiOjAuMjEyNDIxMjQ3ODI1MTYzNywiaWF0IjoxNTk5MTQ5NjE0LCJleHAiOjE2MDAzNTkyMTR9.4rkgZuLtwv5Kh3UlCecLWTccppjcl-_vHuOG7DJnAHE'
+    request1.body = "{\"entryData\":[{\"entryId\":#{entry_id}}]}"
+
+    response1 = http1.request(request1)
+    body1 = JSON.parse(response1.read_body)
+    cloud_key = body1['data']['cloudKeyUnlockToken']
+
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    url2 = URI("https://api.openpath.com/tokens/cloudKeyUnlockTokens/#{cloud_key}/use")
+
+    http2 = Net::HTTP.new(url2.host, url2.port)
+    http2.use_ssl = true
+    http2.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request2 = Net::HTTP::Post.new(url2)
+    request2["content-type"] = 'application/json'
+
+    @response2 = http2.request(request2)
+
+
+
   end
 
   # GET /signups/1
@@ -59,30 +102,29 @@ class SignupsController < ApplicationController
   def create
     @signup = Signup.new(signup_params)
 
-    # @u = params[:username]
-    # @p = params[:password]
+
 # Login as SuperAdmin
-    require 'uri'
-    require 'net/http'
-    require 'openssl'
-    require 'json'
-
-    url = URI("https://api.openpath.com/auth/login")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-    request["accept"] = 'application/json'
-    request["content-type"] = 'application/json'
-    request.body = "{\"email\":\"jeff@recruiterswebsites.com\",\"password\":\"Placement123\"}"
-
-    @response = http.request(request)
-    puts @response.read_body
-
-    body = JSON.parse(@response.read_body)
-    @token = body['data']['token']
+#     require 'uri'
+#     require 'net/http'
+#     require 'openssl'
+#     require 'json'
+#
+#     url = URI("https://api.openpath.com/auth/login")
+#
+#     http = Net::HTTP.new(url.host, url.port)
+#     http.use_ssl = true
+#     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+#     request = Net::HTTP::Post.new(url)
+#     request["accept"] = 'application/json'
+#     request["content-type"] = 'application/json'
+#     request.body = "{\"email\":\"jeff@recruiterswebsites.com\",\"password\":\"Placement123\"}"
+#
+#     @response = http.request(request)
+#     puts @response.read_body
+#
+#     body = JSON.parse(@response.read_body)
+#     @token = body['data']['token']
 
 # Rails local signup
     respond_to do |format|
@@ -95,46 +137,46 @@ class SignupsController < ApplicationController
       end
     end
 # Create User
-    require 'uri'
-    require 'net/http'
-    require 'openssl'
-
-    url = URI("https://api.openpath.com/orgs/3240/users")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-    request["accept"] = 'application/json'
-    request["content-type"] = 'application/json'
-    request["authorization"] = @token
-    email = @signup.email
-    first_name = @signup.first_name
-    last_name = @signup.last_name
-    # password = @signup.password
-    request.body = "{\"identity\":{\"email\":\"#{email}\",\"firstName\":\"#{first_name}\",\"lastName\":\"#{last_name}\"}}"
-    # ,\"password\":\"#{password}\"
-    @response = http.request(request)
-    # puts @response.read_body
-    body = JSON.parse(@response.read_body)
-    @user_id = body['data']['id']
-# Adds user to group
-    require 'uri'
-    require 'net/http'
-    require 'openssl'
-
-    url1 = URI("https://api.openpath.com/orgs/3240/groups/11796/users/#{@user_id}")
-
-    http1 = Net::HTTP.new(url1.host, url1.port)
-    http1.use_ssl = true
-    http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request1 = Net::HTTP::Put.new(url1)
-    request1["authorization"] = @token
-
-    response1 = http1.request(request1)
-    puts response1.read_body
+#     require 'uri'
+#     require 'net/http'
+#     require 'openssl'
+#
+#     url = URI("https://api.openpath.com/orgs/3240/users")
+#
+#     http = Net::HTTP.new(url.host, url.port)
+#     http.use_ssl = true
+#     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+#     request = Net::HTTP::Post.new(url)
+#     request["accept"] = 'application/json'
+#     request["content-type"] = 'application/json'
+#     request["authorization"] = @token
+#     email = @signup.email
+#     first_name = @signup.first_name
+#     last_name = @signup.last_name
+#     # password = @signup.password
+#     request.body = "{\"identity\":{\"email\":\"#{email}\",\"firstName\":\"#{first_name}\",\"lastName\":\"#{last_name}\"}}"
+#     # ,\"password\":\"#{password}\"
+#     @response = http.request(request)
+#     # puts @response.read_body
+#     body = JSON.parse(@response.read_body)
+#     @user_id = body['data']['id']
+# # Adds user to group
+#     require 'uri'
+#     require 'net/http'
+#     require 'openssl'
+#
+#     url1 = URI("https://api.openpath.com/orgs/3240/groups/11796/users/#{@user_id}")
+#
+#     http1 = Net::HTTP.new(url1.host, url1.port)
+#     http1.use_ssl = true
+#     http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+#     request1 = Net::HTTP::Put.new(url1)
+#     request1["authorization"] = @token
+#
+#     response1 = http1.request(request1)
+#     puts response1.read_body
   end
 
   # PATCH/PUT /signups/1
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,2 +1,27 @@
 class Signup < ApplicationRecord
+  has_one_attached :selfie
+  has_one_attached :photo_id
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless selfie.attached?
+
+    unless selfie.byte_size <= 1.megabyte
+      errors.add(:selfie, "is too big")
+    end
+
+    unless photo_id.byte_size <= 1.megabyte
+      errors.add(:photo_id, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(selfie.content_type)
+      errors.add(:selfie, "must be a JPEG or PNG")
+    end
+
+    unless acceptable_types.include?(photo_id.content_type)
+      errors.add(:photo_id, "must be a JPEG or PNG")
+    end
+  end
+
 end
